5a52a30a91196bf7c7cdadb1c5a9647ad9712571
Richie and Robert <glennonr@moravian.edu>
2021-01-23 10:27:43 -0500

    Added removing old dates

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 events = [
-    {"date": "2020-10-16", "distance": 5, "email": "sampleemail1@gmail.com", "location": "sampletown1",
+    {"date": "2021-10-16", "distance": 5, "email": "sampleemail1@gmail.com", "location": "sampletown1",
      "name": "testperson", "pace": "7", "time": "16:00"},
     {"date": "2020-10-18", "distance": 15, "email": "sampleemail2@gmail.com", "location": "sampletown2",
      "name": "testperson2", "pace": "6", "time": "19:00"}
@@ -44,8 +44,10 @@ def clear_events():
 
 
 def delete_past_events():
+
+    today = datetime.datetime.strptime(str(datetime.date.today()), '%Y-%m-%d')
     for event in events:
-        if datetime.datetime.strptime(event['date'], '%Y-%M-%D') < datetime.date.today():
+        if datetime.datetime.strptime(event['date'], '%Y-%m-%d') < today:
             events.remove(event)
 
 
